243dc1018339fad630af55c7a7668a14ffbe90d3
parvizrovshanaliyev <parvizra@code.edu.az>
2019-04-12 21:58:47 +0400

    identity references add

---
+++ Barbeda/Models/AppUser.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Barbeda.Models
+{
+    public class AppUser:IdentityUser
+    {
+          public string Name { get; set; }
+
+          public string Surname { get; set; }
+
+    }
+
+    public class AppRole : IdentityRole
+    {
+        public string Description { get; set; }
+
+
+        public AppRole()
+        {
+
+        }
+
+        public AppRole(string roleName,string description):base(roleName)
+        {
+            this.Description = description;
+        }
+
+    }
+}
